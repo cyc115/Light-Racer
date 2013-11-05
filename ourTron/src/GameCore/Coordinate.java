7cc4f25d8608f1e3c73274b7c88f8b15d6bb9e3c
joannahalpern <joanna.halpern@mail.mcgill.ca>
2013-11-04 23:58:24 -0500

    Added Coordinate

---
+++ ourTron/src/GameCore/Coordinate.java
@@ -0,0 +1,30 @@
+public class Coordinate{
+	private int x;
+	private int y;
+	
+	public Coordinate(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	public int getX(){
+		return x;
+	}
+	public int getY(){
+		return y;
+	}
+	public void setX(int x){
+		this.x =x;
+	}
+	public void setY(int y){
+		this.y =y;
+	}
+	
+	public boolean equals(Coordinate c2){
+		if ((this.x == c2.x) && (this.y == c2.y)){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+}
