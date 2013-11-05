5c48b6d2622b70fe643d3ea6624cf821db5cbbc4
joannahalpern <joanna.halpern@mail.mcgill.ca>
2013-11-05 11:02:45 -0500

    fixed extra }

--- ourTron/src/GameCore/Coordinate.java
+++ ourTron/src/GameCore/Coordinate.java
@@ -28,9 +28,7 @@ public class Coordinate{
 		}
 	}
 
-	    public static void main(String[] args) {
+	public static void main(String[] args) {
 	        System.out.println("It works!");
-	    }
-
 	}
 }
