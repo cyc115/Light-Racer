4b80d74f09888c490d1045d39dcfa199a69a6df1
joannahalpern <joanna.halpern@mail.mcgill.ca>
2013-11-05 00:12:29 -0500

    Added GameScore - constructor now initializes gamescore

---
+++ ourTron/src/GameCore/GameScore.java
@@ -0,0 +1,30 @@
+
+public class GameScore {
+	private int playerOneScore;
+	private int playerTwoScore;
+	
+	public GameScore() {
+		initialize();
+	}
+	public void incrP1Win(){
+		playerOneScore++;
+	}
+	public void incrP2Win(){
+		playerTwoScore++;
+	}
+	public int getRoundNumber(){
+		return playerOneScore + playerTwoScore;
+	}
+	
+	public int getPlayerOneScore() {
+		return playerOneScore;
+	}
+	public int getPlayerTwoScore() {
+		return playerTwoScore;
+	}
+	public void initialize(){
+		this.playerOneScore = 0;
+		this.playerTwoScore = 0;
+	}
+	
+}
