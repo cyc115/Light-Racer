8abcc4f3501e3bceacfd6db242c566373076ffc9
joannahalpern <joanna.halpern@mail.mcgill.ca>
2013-11-05 11:04:50 -0500

    Added EMPTY to MapSign

--- ourTron/src/GameCore/MapSign.java
+++ ourTron/src/GameCore/MapSign.java
@@ -1,4 +1,4 @@
 //player 1 is part of their trail, same with player 2. 
 public enum MapSign {
-	WALL, player1Trail, player2Trail, power1, power2
+	WALL, player1Trail, player2Trail, power1, power2, EMPTY
 }
