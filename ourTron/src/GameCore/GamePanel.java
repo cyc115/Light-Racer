46cb62578a329b7c420b979029fa63ec62096484
joannahalpern <joanna.halpern@mail.mcgill.ca>
2013-11-06 16:09:26 -0500

    Skeleton of GamePanel -none of the methods are filled in

---
+++ ourTron/src/GameCore/GamePanel.java
@@ -0,0 +1,58 @@
+package GameCore;
+
+import Control;
+import GameScore;
+import Map;
+import Player;
+
+public class GamePanel {
+	private Player player1;
+	private Player player2;
+	private GameScore gamescore;
+//	private MusicPlayer soundEffectPlayer
+	private Control direction;
+	private boolean isPaused;
+	private Map gameMap;
+	private int roundNumber;
+	private char userKeypboardInput;
+	
+	
+// Constructor	
+	public GamePanel() {
+		this.isPaused = false;
+		init();
+	}
+	public void onGamePause(){ //TODO: fill in onGamePause
+	}
+	public void onGameResume(){ //TODO: fill in onGameResume
+		while(!isPaused){
+			//listener stuff to get the new direction of player1 and player2
+			
+//			makeTurn(player1, )
+		}
+	
+	}
+	public void init(){ //TODO: fill in init() (right now it's just setup to work for the prototype)
+	gameMap = new Map();
+//	gameMap.createMapFromFile() ...
+	player1 = new Player();
+	player2 = new Player();
+	}
+	public void makeTurn(Player player, Control direction){ //TODO: fill in makeTurn
+	}
+	public void movePlayer(Player player){ //TODO: fill in movePlayer
+	}
+	public int getGameRoundNumber(){ 
+		return roundNumber;
+	}
+	public void endGame(){ //TODO: fill in endGame
+	}
+	public boolean hasCollided(){ //TODO: fill in hasCollided
+	}
+	public void usePowerUp(Player player){ //TODO: fill in usePowerUp
+	}
+	public void obtainPowerUp(){ //TODO: fill in onGamePause
+	}
+
+
+}
