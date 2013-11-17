f294411bdf4d37c81ed5d150e6b356cfde161953
danielle-mustillo <danielle.mustillo@mail.mcgill.ca>
2013-11-16 22:56:51 -0500

    Made UserAuth static as there is only 1 implementation of it.

--- ourTron/src/Backend/UserAuth.java
+++ ourTron/src/Backend/UserAuth.java
@@ -1,11 +1,8 @@
 package Backend;
 
 public class UserAuth {
-	private UserDataBase userDB;
-	
-	public UserAuth(UserDataBase userDB) {
-		this.userDB=userDB;
-	}
+	//unique instance. 
+	private static UserDataBase userDB = new UserDataBase();
 	
 	/**Returns a user if the input of username and password matches the credentials of that user in the database
 	 * <p>
@@ -13,7 +10,7 @@ public class UserAuth {
 	 * @param password	A String object containing the password
 	 * @return	A user object associated to the username if and only if the password is also correct. Otherwise null is returned. 
 	 */
-	public User isValidInput(String username, String password) {
+	public static User isValidInput(String username, String password) {
 		User user = userDB.retrieveUser(username);
 		if(user.getUsername().equals(username) && user.getPassword().equals(password))
 			return user;
@@ -25,7 +22,7 @@ public class UserAuth {
 	 * @param username	A string object contianing the username
 	 * @return	A boolean variable which is true if the username exists in the database. Otherwise false. 
 	 */
-	public boolean isRegistered(String username) {
+	public static boolean isRegistered(String username) {
 		if(userDB.retrieveUser(username) != null)
 			return true;
 		else
