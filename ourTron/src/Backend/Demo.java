cdf3a8d58bd4e5b45d7d1212141249e2cf22be24
joannahalpern <joanna.halpern@mail.mcgill.ca>
2013-11-24 18:26:20 -0500

    Changes to Demo

--- ourTron/src/Backend/Demo.java
+++ ourTron/src/Backend/Demo.java
@@ -2,25 +2,28 @@ package Backend;
 
 public class Demo {
 	public static void main(String[] args) {
-		//create user Demo10
-		User demoUser = new User("Demo10", "Dem@Us3R10");
+//		//create user Demo10
+//		User demoUser = new User("Demo10", "Dem@Us3R10");
+//		
+//		//create demo users 1-9
+//		for(int i=1; i<=9; i++){
+//			demoUser = new User("Demo0"+ i, "Dem@Us3R0"+ i);
+//			System.out.println(demoUser.getUsername());
+//			System.out.println(demoUser.getPassword());
+//		}
+//		User user1 = new User();
+//		user1 = UserDataBase.retrieveUser("Demo01");
 		
-		//create demo users 1-9
-		for(int i=1; i<=9; i++){
-			demoUser = new User("Demo0"+ i, "Dem@Us3R0"+ i);
-			System.out.println(demoUser.getUsername());
-		}
-		addDemoUserHistory("Joanna", "Halpern", 2, 3);
 		
 		//add the demo scores specified by the prof
-//		addDemoUserHistory("Demo01", "Demo02", 2, 2);
-//		addDemoUserHistory("Demo01", "Demo03", 2, 3);
-//		addDemoUserHistory("Demo01", "Demo04", 2, 4);
-//		addDemoUserHistory("Demo01", "Demo05", 2, 5);
-//		addDemoUserHistory("Demo06", "Demo07", 2, 2);
-//		addDemoUserHistory("Demo06", "Demo08", 2, 3);
-//		addDemoUserHistory("Demo06", "Demo09", 2, 4);
-//		addDemoUserHistory("Demo06", "Demo10", 2, 5);
+		addDemoUserHistory("Demo01", "Demo02", 1, 2);
+		addDemoUserHistory("Demo01", "Demo03", 2, 3);
+		addDemoUserHistory("Demo01", "Demo04", 2, 4);
+		addDemoUserHistory("Demo01", "Demo05", 2, 5);
+		addDemoUserHistory("Demo06", "Demo07", 2, 2);
+		addDemoUserHistory("Demo06", "Demo08", 2, 3);
+		addDemoUserHistory("Demo06", "Demo09", 2, 4);
+		addDemoUserHistory("Demo06", "Demo10", 2, 5);
 	}
 	
 	
@@ -37,14 +40,14 @@ public class Demo {
 			user1 = UserDataBase.retrieveUser(user1Name);
 			User user2 = new User();
 			user2 =	UserDataBase.retrieveUser(user2Name);
-//			for(int i=0; i<user1Wins; i++){
+			for(int i=0; i<user1Wins; i++){
 				user1.addGameResult(user2, true);
-//				user2.addGameResult(user1, false);
-//			}
-//			for(int i=0; i<user2Wins; i++){
-//				user1.addGameResult(user2, false);
-//				user2.addGameResult(user1, true);
-//			}
+				user2.addGameResult(user1, false);
+			}
+			for(int i=0; i<user2Wins; i++){
+				user1.addGameResult(user2, false);
+				user2.addGameResult(user1, true);
+			}
 		}
 	}
 }
