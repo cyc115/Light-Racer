5a292a4624ff546091a901bf7744a328bc82b218
Danielle Mustillo <danielle.mustillo@mail.mcgill.ca>
2013-11-27 21:39:21 -0500

    Updated the javadocs and comments.

--- ourTron/src/Backend/UserAuth.java
+++ ourTron/src/Backend/UserAuth.java
@@ -1,34 +1,56 @@
 package Backend;
 
 public class UserAuth {
-	//unique instance. 
-	private static UserDataBase userDB = new UserDataBase();
-	
-	/**Returns a user if the input of username and password matches the credentials of that user in the database
+	/**
+	 * This static method will compare the user credentials passed into this
+	 * method against that in the database. It will attempt to find a
+	 * {@link User} with a matching username and, if successful, will identify
+	 * if the password in the database matches that which is input. It will
+	 * return the instance of the user in the database.
 	 * <p>
-	 * @param username	A String object containing the username
-	 * @param password	A String object containing the password
-	 * @return	A user object associated to the username if and only if the password is also correct. Otherwise null is returned. 
+	 * This method does not guarantee results if the user does not exist in the
+	 * database. That functionality is handled by the method isRegistered().
+	 * This method also does not guarantee results if more than one user share
+	 * the same username.
+	 * 
+	 * @param username
+	 *            A String object containing the username
+	 * @param password
+	 *            A String object containing the password
+	 * @return Returns a {@link User} object if the input of username and
+	 *         password matches the credentials of that user in the database.
+	 *         Otherwise null is returned.
 	 */
 	public static User isValidInput(String username, String password) {
 		User user = UserDataBase.retrieveUser(username);
-		if(user.getUsername().equals(username) && user.getPassword().equals(password)) {
+		// compare database credentials to parameters
+		if (user.getUsername().equals(username)
+				&& user.getPassword().equals(password)) {
 			return user;
-		}
-		else {
+		} else {
 			return null;
 		}
 	}
-	
-	/**Returns whether the username exists in the database
-	 * @param username	A string object contianing the username
-	 * @return	A boolean variable which is true if the username exists in the database. Otherwise false. 
+
+	/**
+	 * This static method will compare the username input with all the
+	 * {@link User} objects in the database in order to find a match. If a user
+	 * with the matching username is found, the method return true.
+	 * <p>
+	 * This method assumes the unique identifier to every user is the username.
+	 * It will not guarantee results if more than one user share an identical
+	 * username.
+	 * 
+	 * @param username
+	 *            A string object containing the username
+	 * @return A boolean variable which is true if the username exists in the
+	 *         database. Otherwise false.
 	 */
 	public static boolean isRegistered(String username) {
-		if(UserDataBase.retrieveUser(username) != null) {
+		// null means the user does not exist in the database.
+		if (UserDataBase.retrieveUser(username) != null) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
